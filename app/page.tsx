eba7bc6c127c4fdfc54d53fba988785e316631e0
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-12-01 17:29:18 +0700

    connect to esp32 board data

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
 "use client"
-import { get, push, ref, set } from "firebase/database"
+// import { push, set } from "firebase/database"
+import { get, ref } from "firebase/database"
 import { useState, useEffect } from "react"
 import { database } from "./firebase";
 
-interface User {
-  id?: string;
-  data?: string;
+interface SensorValues {
+  ldrValue: number;
+  micValue: number;
+  mq2AnalogValue: number;
+  mq2DigitalValue: number;
+  t447Value: number;
+  temperature: number;
 }
 
 export default function Home() {
-  const [data, setData] = useState("");
-  const [users, setUsers] = useState<User[]>([]);
+  // const [data, setData] = useState("");
+  const [data, setData] = useState<SensorValues | null>(null);
 
   useEffect(() => {
-    const usersRef = ref(database, "users");
-    get(usersRef).then((snapshot) => {
+    const sensorValueRef = ref(database, "sensorValue");
+    get(sensorValueRef).then((snapshot) => {
       if (snapshot.exists()) {
-        const usersArray = Object.entries(snapshot.val()).map(([id, userData]) => {
-          if (typeof userData === 'object' && userData !== null) {
-            return {
-              id,
-              ...userData,
-            };
-          } else {
-            console.error(`Invalid user data for id: ${id}`, userData);
-            return { id, data: "Invalid data" };
-          }
-        });
-        setUsers(usersArray);
+        setData(snapshot.val());
       } else {
         console.error("No data available");
       }
@@ -36,52 +30,53 @@ export default function Home() {
     });
   }, []);
 
-  const handleAddData = () => {
-    try {
-      const usersRef = ref(database, "users");
-      const newDataRef = push(usersRef);
+  // const handleAddData = () => {
+  //   try {
+  //     const usersRef = ref(database, "users");
+  //     const newDataRef = push(usersRef);
 
-      set(newDataRef, {
-        data: data,
-      });
-      setData("");
-      alert("Data added successfully");
-    } catch (error) {
-      console.error("Error adding data: ", error);
-    }
-  }
+  //     set(newDataRef, {
+  //       data: data,
+  //     });
+  //     setData("");
+  //     alert("Data added successfully");
+  //   } catch (error) {
+  //     console.error("Error adding data: ", error);
+  //   }
+  // }
   return (
     <div className="flex min-h-screen flex-col items-center p-12">
       <h1 className="text-4xl font-bold text-center my-10">
-        Add Data to Firebase Realtime Database
-      </h1>
-      <div className="mb-4">
-        <input 
-          type="text" 
-          placeholder="Enter Data"
-          value={data}
-          onChange={(e) => setData(e.target.value)}
-          className="w-full border p-2"
-        />
-      </div>
-      <button
-        onClick={handleAddData}
-        className="bg-blue-500 text-white p-2 rounded-md"
-      >
-        Add Data
-      </button>
-      <h1 className="text-4xl font-bold text-center my-10">
         Fetch Data from Firebase Realtime Database
       </h1>
-      <div className="grid grid-cols-3 gap-4">
-        {users.map((user) => (
-          <div key={user.id} className="bg-gray-100 p-4 rounded-lg">
-            <h2 className="text-2xl text-gray-900">
-              {user.data}
-            </h2>
-          </div>
-        ))}
-      </div>
+      {data ? (
+        <div className="grid grid-cols-3 gap-4">
+          <>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">LDR Value: {data.ldrValue}</h2>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">Mic Value: {data.micValue}</h2>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">MQ2 Analog Value: {data.mq2AnalogValue}</h2>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">MQ2 Digital Value: {data.mq2DigitalValue}</h2>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">T447 Value: {data.t447Value}</h2>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2 className="text-2xl text-gray-900">Temperature: {data.temperature}</h2>
+            </div>
+          </>
+        </div>
+      ) : (
+        <p className="text-2xl font-bold text-center my-10">
+          Loading data...
+        </p>
+      )}
     </div>
   );
 }
